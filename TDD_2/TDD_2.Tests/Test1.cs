42715e3825820e59d3b681348e0e3314df245893
sz-99 <zhengsusan3@gmail.com>
2024-11-18 15:10:16 +0000

    added reverse string method and test

--- TDD_2/TDD_2.Tests/Test1.cs
+++ TDD_2/TDD_2.Tests/Test1.cs
@@ -10,7 +10,7 @@ namespace TDD_2.Tests
         }
 
         [Test, Description("Organisation!")]
-        public void Test1()
+        public void TestSingleDirection()
         {
             Compass greatCompass = new Compass();
 
@@ -23,20 +23,73 @@ namespace TDD_2.Tests
         }
 
         [Test]
-        public void Test2()
+        public void TestRotateRight()
         {
             Compass compass = new Compass();
-            Point result1 = compass.Rotate(Point.East, Direction.left);
-            Point result2 = compass.Rotate(Point.North, Direction.left);
-            Point result3 = compass.Rotate(Point.West, Direction.left);
-            Point result4 = compass.Rotate(Point.South, Direction.left);
+            //Point result1 = compass.Rotate(Point.East, Direction.left);
+            //Point result2 = compass.Rotate(Point.North, Direction.left);
+            //Point result3 = compass.Rotate(Point.West, Direction.left);
+            //Point result4 = compass.Rotate(Point.South, Direction.left);
+            //Assert.Multiple(() =>
+            //{
+            //    result1.Should().Be(Point.North);
+            //    result2.Should().Be(Point.West);
+            //    result3.Should().Be(Point.South);
+            //    result4.Should().Be(Point.East);
+            //});
+
+            var testCases = new (Point point, Direction direction, Point expectedResult)[]
+                {
+                (Point.North, Direction.right, Point.East),
+                (Point.East, Direction.right, Point.South),
+                (Point.South, Direction.right, Point.West),
+                (Point.West, Direction.right, Point.North),
+                };
+
             Assert.Multiple(() =>
             {
-                result1.Should().Be(Point.North);
-                result1.Should().Be(Point.West);
-                result1.Should().Be(Point.South);
-                result1.Should().Be(Point.East);
+                foreach (var (a, b, expected) in testCases)
+                {
+                    Point result = compass.Rotate((Point)a, (Direction)b);
+                    result.Should().Be(expected);
+                }
             });
+        }
+
+
+        [Test]
+        public void TestRotateLeft()
+        {
+            Compass compass = new Compass();
+            var testCases = new (Point point, Direction direction, Point expectedResult)[]
+                {
+                (Point.North, Direction.left, Point.West),
+                (Point.East, Direction.left, Point.North),
+                (Point.South, Direction.left, Point.East),
+                (Point.West, Direction.left, Point.South),
+                };
+
+            Assert.Multiple(() =>
+            {
+                foreach (var (a, b, expected) in testCases)
+                {
+                    Point result = compass.Rotate((Point)a, (Direction)b);
+                    result.Should().Be(expected);
+                }
+            });
+
+
+        }
+
+        [Test]
+        public void TestReverseString()
+        {
+            string testString = "hello";
+            var stringManipulator = new StringManipulator();
+
+            string result = stringManipulator.ReverseString(testString);
+
+            result.Should().Be("olleh");
         }
     }
 }
---
+++ TDD_2/TDD_2/StringManipulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_2
+{
+    
+        public class StringManipulator
+        {
+            public string ReverseString(string input)
+            {
+            // TODO: Implement the logic to reverse the input string
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+                return new string(chars);
+            }
+
+            public bool IsPalindrome(string input)
+            {
+                // TODO: Implement the logic to check if the input string is a palindrome
+                // (A palindrome = same forwards as backwards)
+                return false;
+            }
+        }
+    
+}
