8e598c6fe485fbe28cd674797c95c1c689b1f37f
Joseph Eaton <josepheaton04@outlook.com>
2024-11-18 15:16:48 +0000

    Add wordanalyser skeleton class

---
+++ TDD_2/TDD_2/WordAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_2
+{
+    public class WordAnalyser
+    {
+        public List<string> FindLongestWords(string text)
+        {
+            // TODO: Implement the logic to find the longest word(s) in the given text
+            return new List<string>();
+        }
+
+        public Dictionary<char, int> CalculateLetterFrequency(string text)
+        {
+            // TODO: Implement the logic to calculate the frequency of each letter in the given text
+            return new Dictionary<char, int>();
+        }
+    }
+}
