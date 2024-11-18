b6af90c2174e9af9be1c95fe2701e12a6ffa3185
sz-99 <zhengsusan3@gmail.com>
2024-11-18 10:59:32 +0000

    'add point and direction enum; add compass class'

---
+++ TDD_2/TDD_2/Compass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_2
+{
+    internal class Compass
+    {
+        public Point Point { get; set; }
+
+        public Point Rotate(Point point, Direction direction)
+        {
+            return Point.South;
+        }
+    }
+}
---
+++ TDD_2/TDD_2/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_2
+{
+    public enum Point
+    {
+        North, East, South, West
+    }
+}
